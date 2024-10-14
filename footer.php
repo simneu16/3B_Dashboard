9ed10f5d418c388ff1abebd8ebfa4f6e4c2ddb52
simneu16 <165833687+simneu16@users.noreply.github.com>
2024-10-14 19:18:15 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; <?php echo $pageName; ?></span>
+            <span>Copyright &copy; <?php echo strtoupper($pageName); ?></span>
         </div>
     </div>
 </footer>
